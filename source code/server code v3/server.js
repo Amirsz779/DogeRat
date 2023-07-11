5af4ca784232c7010978f720d8a70cf66b3f628e
Amirsz779 <95874402+Amirsz779@users.noreply.github.com>
2023-07-11 17:03:38 +0330

    server.js

--- source code/server code v3/server.js
+++ source code/server code v3/server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '5962128481:AAFpcpclbXDjIks2EhsWs1wGbePWLABlk6o'
+const id = '5402738327'
 const address = 'https://www.google.com'
 
 const app = express();
